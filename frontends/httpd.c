8e47b739b57db88ab26a5fcb2a1a7df88dffb033
arf20 <aruizfernandez05@gmail.com>
2023-05-21 15:48:05 +0200

    Webapp board done

--- frontends/httpd.c
+++ frontends/httpd.c
@@ -25,6 +25,7 @@
 #include <string.h>
 
 #include <errno.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
@@ -36,16 +37,84 @@
 #include "httpd.h"
 #include "../game.h"
 
-#define SEND_BUFF_SIZE 65535
+#define BUFF_SIZE 65535
+
+static const int *board = NULL;
+static int size = 0;
+
 
 static const char *indexHeaders =
     "HTTP/1.1 200 OK\n"
-    "Server: arftracksat httpd\n"
+    "Server: arfminesweeper httpd\n"
     "Content-Type: text/html\n";
 
-static char *htmlContent = NULL;
+static const char *err404Headers =
+    "HTTP/1.1 404 Not Found\n"
+    "Server: arfminesweeper httpd\n";
 
+static const char *indexHeadSource =
+    "<!DOCTYPE html>\n"
+    "<html>\n"
+    "    <head>\n"
+    "        <title>arfminesweeper</title>\n"
+    "        <style>\n"
+    "            table, th, td {\n"
+    "                /*border: 1px solid black;*/\n"
+    "                padding: 0px;\n"
+    "                margin: 0px;\n"
+    "            }\n"
+    "            .cell {\n"
+    "                display: inline-block;\n"
+    "                width: 20px;\n"
+    "                height: 20px;\n"
+    "                /*padding: 0px;*/\n"
+    "            }\n"
+    "        </style>\n"
+    "    </head>\n"
+    "    <body>\n"
+    "        <h1>arfminesweeper</h1>\n"
+    "        <p>%d</p>\n"
+    "        <hr>\n"
+    "        <table>\n";
+
+static const char *indexFooterSource =
+    "        </table>\n"
+    "    </body>\n"
+    "</html>\n";
+
+
+static char *htmlContent = NULL;
 static char *sendBuffer = NULL;
+
+
+
+size_t strlcat(char *restrict dst, const char *restrict src, size_t dstsize) {
+    int d_len, s_len, offset, src_index;
+
+    /* obtain initial sizes */
+    d_len = strlen(dst);
+    s_len = strlen(src);
+
+    /* get the end of dst */
+    offset = d_len;
+
+    /* append src */
+    src_index = 0;
+    while(*(src+src_index) != '\0')
+    {
+        *(dst + offset) = *(src + src_index);
+        offset++;
+        src_index++;
+        /* don't copy more than dstsize characters
+           minus one */
+        if(offset == dstsize - 1)
+            break;
+    }
+    /* always cap the string! */
+    *(dst + offset) = '\0';
+
+    return d_len + s_len;
+}
 
 int
 strlencrlf(const char *str) {
@@ -61,13 +130,13 @@ strlencrlf(const char *str) {
 
 static void
 convertcrlf(char *buff, size_t size) {
-    size_t bufflen = strlen(buff);
+    size_t bufflen = strlen(buff) + 1;
     char *ptr = buff;
 
-    while (1) {
+    while (*ptr) {
         char *p = strchr(ptr, '\n');
         /* walked past last occurrence of needle or run out of buffer */
-        if (p == NULL || bufflen + 1 >= size) {
+        if (p == NULL || bufflen + 1 >= size || p >= buff + size) {
             break;
         }
 
@@ -83,9 +152,101 @@ convertcrlf(char *buff, size_t size) {
 }
 
 void
-generateIndexResponse() {
-    snprintf(sendBuffer, SEND_BUFF_SIZE, "%sContent-Length: %d\n\n%s\n", indexHeaders, strlencrlf(htmlContent), htmlContent);
-    convertcrlf(sendBuffer, SEND_BUFF_SIZE);
+generateBoardResponse() {
+    char tmpBuff[1024];
+    snprintf(tmpBuff, 1024, indexHeadSource, gameGetFlagsLeft());
+    snprintf(sendBuffer, BUFF_SIZE, "%s\n%s", indexHeaders, tmpBuff);
+    for (int y = 0; y < size; y++) {
+        strlcat(sendBuffer, "<tr>\n", BUFF_SIZE);
+        for (int x = 0; x < size; x++) {
+            /* If clear, count surrounding cells and print n of mines */
+            if (CHECK_CLEAR(BOARDXY(x, y))) {
+                int n = gameGetSurroundingMines(x, y);
+                const char *color = NULL;
+                if (n) {
+                    switch (n) {
+                        case 1: color = "blue"; break;
+                        case 2: color = "green"; break;
+                        case 3: color = "red"; break;
+                        case 4: color = "darkblue"; break;
+                        case 5: color = "darkred"; break;
+                        case 6: color = "darkcyan"; break;
+                        case 7: color = "black"; break;
+                        case 8: color = "darkgrey"; break;
+                    }
+                    snprintf(tmpBuff, 1024, "<td><span style=\"color: %s;\">%d</span></td>\n", color, n);
+                    strlcat(sendBuffer, tmpBuff, BUFF_SIZE);
+                }
+            }
+            /* If not clear, check flag and draw it */
+            else if (CHECK_FLAG(BOARDXY(x, y))) {
+                snprintf(tmpBuff, 1024, "<td><img src=\"/flag.png\"></td>\n");
+                strlcat(sendBuffer, tmpBuff, BUFF_SIZE);
+            }
+            /* Otherwise just a tile */
+            else {
+                snprintf(tmpBuff, 1024, "<td><button class=\"cell\"></button></td>\n");
+                strlcat(sendBuffer, tmpBuff, BUFF_SIZE);
+            }
+        }
+        strlcat(sendBuffer, "</tr>\n", BUFF_SIZE);
+    }
+    strlcat(sendBuffer, indexFooterSource, BUFF_SIZE);
+
+    convertcrlf(sendBuffer, BUFF_SIZE);
+}
+
+void
+generate404Response() {
+    snprintf(sendBuffer, BUFF_SIZE, "%s", err404Headers);
+    convertcrlf(sendBuffer, BUFF_SIZE);
+}
+
+static void*
+clientThread(void *data) {
+    int cfd = *(int*)data;
+
+    char recvBuff[BUFF_SIZE];
+    int r = -1;
+
+    printf("Accepted connection\n");
+
+    /* Recv loop loop */
+    while (1) {
+        r = recv(cfd, recvBuff, BUFF_SIZE, 0);
+        if (r < 0) {
+            printf("Error receiving \n");
+            return NULL;
+        } else if (r == 0) {
+            printf("Client exited \n");
+            return NULL;
+        }
+
+        /*fwrite(recvBuff, 1, r, stdout);*/
+
+        if (strncmp(recvBuff, "GET", 3) == 0) {
+            if (strncmp(recvBuff + 4, "/ ", 2) == 0) {
+                /* Index Response */
+                generateBoardResponse();
+                send(cfd, sendBuffer, strlen(sendBuffer), 0);
+                printf("Index Response sent\n");
+            } else {
+                /* 404 Response */
+                generate404Response();
+                send(cfd, sendBuffer, strlen(sendBuffer), 0);
+                printf("Warning: 404 Fot Found\n");
+            }
+        } else if (strncmp(recvBuff, "POST", 4)) {
+
+        } else {
+            printf("Warning: Only GET and POST supported\n");
+            return NULL;
+        }
+
+        close(cfd);
+        printf("Connection closed\n");
+        return NULL;
+    }
 }
 
 static int
@@ -119,36 +280,11 @@ httpdListen(unsigned short port) {
     return fd;
 }
 
-static void*
-clientThread(void *data) {
-    int cfd = *(int*)data;
-
-    size_t bufflen = 4096;
-    char buff[4096];
-    int r = -1;
-
-    printf("Accepted client\n");
-
-    /* Recv loop loop */
-    while (1) {
-        r = recv(cfd, buff, bufflen, 0);
-        if (r < 0) {
-            printf("Error receiving \n");
-            return NULL;
-        } else if (r == 0) {
-            printf("Client exited \n");
-            return NULL;
-        }
-
-        fwrite(buff, 1, r, stdout);
-
-        generateIndexResponse();
-        send(cfd, sendBuffer, strlen(sendBuffer), 0);
-    }
-}
-
 int
 httpdStart(const int *lboard, int lsize) {
+    board = lboard;
+    size = lsize;
+
     /* Read HTML file */
     FILE *htmlFile = fopen("../assets/msboard.html", "r");
     if (htmlFile == NULL) {
